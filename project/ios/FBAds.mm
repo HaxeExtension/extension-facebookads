94d908ead1f2772e66b578e668069cc183033809
Federico Bricker <fbricker@gmail.com>
2015-12-04 12:58:38 -0300

    Add banner implementation for iOS

--- project/ios/FBAds.mm
+++ project/ios/FBAds.mm
@@ -4,29 +4,47 @@
 namespace facebookadsex {
 	
 	static FBInterstitialView *interstitial;
+	static FBAdView *bannerView;
 	
 	void init(const char *__BannerID, const char *__InterstitialID, bool alignTop, bool testingAds){
 		NSString *bannerID = [NSString stringWithUTF8String:__BannerID];
 		NSString *interstitialID = [NSString stringWithUTF8String:__InterstitialID];
+		UIViewController *root = [[[UIApplication sharedApplication] keyWindow] rootViewController];
 
 		if(testingAds){
 			//[FBAdSettings addTestDevice:@"HASHED ID"];
 		}
 
+		bannerView = [[FBAdView alloc] initWithPlacementID:bannerID
+									adSize:kFBAdSizeHeight50Banner
+									rootViewController:root];
+
+		[bannerView loadAd];
+		[root.view addSubview:bannerView];
+
 		interstitial = [[FBInterstitialView alloc] initWithID:interstitialID];
 		[interstitial loadInterstital];
+
+		// THOSE THREE LINES ARE FOR SETTING THE BANNER BOTTOM ALIGNED
+		if(!alignTop){
+			CGRect frame = bannerView.frame;
+			frame.origin.y = root.view.bounds.size.height - frame.size.height;
+			bannerView.frame=frame;
+		}
+
+		hideBanner();
 	}
 	
 	void showBanner(){
-//		bannerView.hidden=false;
+		bannerView.hidden=false;
 	}
 	
 	void hideBanner(){
-//		bannerView.hidden=true;
+		bannerView.hidden=true;
 	}
 	
   	void refreshBanner(){
-//		[bannerView loadRequest:[GADRequest request]];
+		// [bannerView loadRequest:[GADRequest request]];
 	}
 
 	bool showInterstitial(){
